fd238303cae1752756359dafd5b23b0b94aba697
riddhi-z1465 <riddhizunjarrao669@gmail.com>
2025-09-17 15:12:01 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+// Config firebase 
+export const firebaseConfig = {
+    apiKey: "AIzaSyClWy8QxcBmRmviDII53Jj0T7xLj3X6iIY",
+    authDomain: "riddhi-6f342.firebaseapp.com",
+    databaseURL: "https://riddhi-6f342-default-rtdb.firebaseio.com",
+    projectId: "riddhi-6f342",
+    storageBucket: "riddhi-6f342.appspot.com",
+    messagingSenderId: "276449717835",
+    appId: "1:276449717835:web:4440f70f3d6274441867ce",
+    measurementId: "G-N42TGQQ6P2"
+};
